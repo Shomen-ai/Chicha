5d7f0cca60d2aa9c344c3ac09962c8ef54a00c3a
Shomen-ai <jthehuman37@yandex.ru>
2024-02-23 21:10:04 +0300

    Initial Commit

---
+++ Chicha/ChichaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChichaApp.swift
+//  Chicha
+//
+//  Created by Дмитрий Шайманов on 23.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChichaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
